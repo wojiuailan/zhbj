9bf903574e6c0b5d4668fbbdd8d687efdd3d3304
wojiuailan <1826248452@qq.com>
2018-06-05 17:22:51 +0800

    .

--- app/src/main/java/xin/shengnan/zhbj/SplashActivity.java
+++ app/src/main/java/xin/shengnan/zhbj/SplashActivity.java
@@ -29,10 +29,16 @@ public class SplashActivity extends AppCompatActivity {
         ivSplash = findViewById(R.id.iv_splash);
     }
 
+    /**
+     * 设置动画
+     * */
     private void setAnimation() {
         AlphaAnimation alphaAnimation = new AlphaAnimation(0f, 1f);
-        ScaleAnimation scaleAnimation = new ScaleAnimation(0.5f, 1f, 0.5f, 1f);
-        RotateAnimation rotateAnimation = new RotateAnimation(0.5f, 1f);
+        alphaAnimation.setDuration(2000);
+        ScaleAnimation scaleAnimation = new ScaleAnimation(0.5f, 1f, 0.5f, 1f, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+        scaleAnimation.setDuration(1000);
+        RotateAnimation rotateAnimation = new RotateAnimation(300f, 360f, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+        rotateAnimation.setDuration(1000);
 
         AnimationSet animationSet = new AnimationSet(true);
         animationSet.addAnimation(alphaAnimation);
