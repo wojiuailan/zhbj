15965f084ef577017a0c9ad01cfbf18b143ff1a5
wojiuailan <1826248452@qq.com>
2018-06-05 16:55:07 +0800

    .

--- app/src/main/java/xin/shengnan/zhbj/SplashActivity.java
+++ app/src/main/java/xin/shengnan/zhbj/SplashActivity.java
@@ -2,12 +2,43 @@ package xin.shengnan.zhbj;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.view.animation.AnimationSet;
+import android.view.animation.RotateAnimation;
+import android.view.animation.ScaleAnimation;
+import android.widget.ImageView;
 
 public class SplashActivity extends AppCompatActivity {
+
+    private ImageView ivSplash;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        initUI();
+        setAnimation();
+    }
+
+    /**
+     * 初始化UI控件
+     * */
+    private void initUI() {
+        ivSplash = findViewById(R.id.iv_splash);
+    }
+
+    private void setAnimation() {
+        AlphaAnimation alphaAnimation = new AlphaAnimation(0f, 1f);
+        ScaleAnimation scaleAnimation = new ScaleAnimation(0.5f, 1f, 0.5f, 1f);
+        RotateAnimation rotateAnimation = new RotateAnimation(0.5f, 1f);
+
+        AnimationSet animationSet = new AnimationSet(true);
+        animationSet.addAnimation(alphaAnimation);
+        animationSet.addAnimation(scaleAnimation);
+        animationSet.addAnimation(rotateAnimation);
+
+        ivSplash.setAnimation(animationSet);
     }
 }
